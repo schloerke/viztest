09c61cfbe75e9427fbf159e2e1354b4e7a66dcf8
Barret Schloerke <schloerke@gmail.com>
2018-03-01 09:13:27 -0800

    have viz_compare work with output folder

--- R/diff.R
+++ R/diff.R
@@ -1,8 +1,24 @@
 
 #' Compare two output folders
 #'
-#' @param output_a,output_b folders to compare that contain output images
+#' @param output_dir Output folder created by \code{viztest}
+#' @param browse If \code{TRUE}, diff.html in \code{output_dir} will be opened
 #' @export
-viz_compare <- function(output_a, output_b) {
-  shinytest::diffviewer_widget(output_a, output_b)
+#' @importFrom utils browseURL
+viz_compare <- function(output_dir, browse = TRUE) {
+  withr::with_dir(output_dir, {
+    cran_images <- file.path(rel_cran_dir, rel_images_dir)
+    local_images <- file.path(rel_local_dir, rel_images_dir)
+
+    ret <- shinytest::diffviewer_widget(cran_images, local_images)
+    # message("Saving diff.html output")
+    # if selfcontained is TRUE, it takes fooorrreeeeevvvveeerrr
+    htmlwidgets::saveWidget(ret, "diff.html", selfcontained = FALSE)
+
+    if(isTRUE(browse)) {
+      browseURL("diff.html")
+    }
+
+    invisible(ret)
+  })
 }
